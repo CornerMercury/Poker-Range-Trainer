f26657562a065f936b414e43a5e074baf53638f5
corner <rileylnorton05@gmail.com>
2024-02-02 20:02:24 +0000

    Almost added swipe ranges

--- src/screens/RangesScreen.js
+++ src/screens/RangesScreen.js
@@ -1,41 +1,57 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import {
 	StyleSheet,
 	Text,
 	View,
 	TouchableOpacity,
 	Dimensions,
+	PanResponder,
+	Animated,
 } from "react-native";
 import { POKER_CHART } from "../constants/constants";
 const { width } = Dimensions.get("window");
 const CELL_SIZE = width / 13;
+
+const calculateIndicesFromCoordinates = (x, y) => {
+	// Assuming the grid starts at position (0, 0) and the top-left corner of the first cell is at (0, 0)
+	const rowIndex = Math.floor(y / CELL_SIZE);
+	const colIndex = Math.floor(x / CELL_SIZE);
+
+	if (rowIndex >= 0 && rowIndex < 13 && colIndex >= 0 && colIndex < 13) {
+		return { rowIndex, colIndex };
+	} else {
+		return null;
+	}
+};
 
 const RangesScreen = () => {
 	const [gridColors, setGridColors] = useState(
 		Array(13).fill(Array(13).fill("blue"))
 	);
 
-	const handleTouchMove = (evt, row, col) => {
-		const { locationX, locationY } = evt.nativeEvent;
-		const newRow = Math.floor((locationY / GRID_SIZE) * 13);
-		const newCol = Math.floor((locationX / GRID_SIZE) * 13);
+	const panResponder = useMemo(() =>
+		PanResponder.create(
+			{
+				onStartShouldSetPanResponder: () => true,
+				onPanResponderMove: (_, gestureState) => {
+					const { moveX, moveY } = gestureState;
+					const touchedIndices = calculateIndicesFromCoordinates(moveX, moveY);
 
-		if (
-			newRow >= 0 &&
-			newRow < gridColors.length &&
-			newCol >= 0 &&
-			newCol < gridColors[0].length &&
-			gridColors[newRow][newCol] !== "red"
-		) {
-			const newGridColors = gridColors.map((rowColors, rowIndex) =>
-				rowColors.map((color, colIndex) =>
-					rowIndex === newRow && colIndex === newCol ? "red" : color
-				)
-			);
-			setGridColors(newGridColors);
-		}
-	};
-	// Create a function to generate the poker chart
+					if (touchedIndices) {
+						const { rowIndex, colIndex } = touchedIndices;
+						const updatedColors = gridColors.map((row) => [...row]);
+						console.log(updatedColors.flat().filter((elem) => elem == "red"));
+						updatedColors[rowIndex][colIndex] = "red";
+						setGridColors(updatedColors);
+					}
+				},
+				onPanResponderRelease: () => {
+					// handle release event if needed
+				},
+			},
+			[gridColors]
+		)
+	).current;
 	const renderChart = () => {
 		const rows = [];
 		for (let i = 0; i < 13; i++) {
@@ -43,13 +59,15 @@ const RangesScreen = () => {
 			for (let j = 0; j < 13; j++) {
 				const hand = POKER_CHART[i][j];
 				cols.push(
-					<TouchableOpacity
+					<Animated.View
 						key={hand}
 						style={[styles.cell, { backgroundColor: gridColors[i][j] }]}
-						onTouchMove={(evt) => handleTouchMove(evt, rowIndex, colIndex)}
+						{...panResponder.panHandlers}
 					>
-						<Text style={styles.text}>{hand}</Text>
-					</TouchableOpacity>
+						<TouchableOpacity>
+							<Text style={styles.text}>{hand}</Text>
+						</TouchableOpacity>
+					</Animated.View>
 				);
 			}
 			rows.push(
